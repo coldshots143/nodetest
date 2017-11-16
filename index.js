735a5c21c614bc62abc0ab851c937769fc8eb077
c07d-736734 <coldshots143@gmail.com>
2017-11-16 20:37:04 +0800

    server test

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res){
 res.writeHead(200, {'Content-Type': 'text/plain'});
 res.end('Hello World\n');
-}).listen(8800, '192.168.1.43');
+}).listen('192.168.1.43');
 console.log('Server running at http://192.168.1.43:8800/'); 
